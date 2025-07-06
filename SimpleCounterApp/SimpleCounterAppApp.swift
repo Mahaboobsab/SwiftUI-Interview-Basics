75d5cf3ee7f684da4293fc9791d391b1c8e66e2a
Mahaboobsab <git config --global user.email YourEmailProvidedToGit@Somedomain.com>
2025-07-06 21:55:34 +0530

    Initial Commit

---
+++ SimpleCounterApp/SimpleCounterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleCounterAppApp.swift
+//  SimpleCounterApp
+//
+//  Created by Alkit Gupta on 06/07/25.
+//
+
+import SwiftUI
+
+@main
+struct SimpleCounterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
